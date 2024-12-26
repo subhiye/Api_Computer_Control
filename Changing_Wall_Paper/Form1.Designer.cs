7fe2e01f839e8e08c725be56f4eba40b7f0b1bb0
Siba Sbahi <ahmadmerhaba1991@gmail.com>
2024-12-26 20:46:40 +0300

    Initial commit

---
+++ Changing_Wall_Paper/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Changing_Wall_Paper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.lblHieght = new System.Windows.Forms.Label();
+            this.lblBattery = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Battery_Life_Remaining = new System.Windows.Forms.Label();
+            this.Battery_Charge_Status = new System.Windows.Forms.Label();
+            this.lblAcLine = new System.Windows.Forms.Label();
+            this.Full_Battery_Lifetime = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lblCloseComputer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(349, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Change";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(3, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(326, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Change Windows Wall Paper";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(386, 383);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(237, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Hieght Of The Computer Screen";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(12, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(231, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Width Of The Computer Screen";
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWidth.ForeColor = System.Drawing.Color.Blue;
+            this.lblWidth.Location = new System.Drawing.Point(262, 83);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(72, 25);
+            this.lblWidth.TabIndex = 6;
+            this.lblWidth.Text = "Width";
+            // 
+            // lblHieght
+            // 
+            this.lblHieght.AutoSize = true;
+            this.lblHieght.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHieght.ForeColor = System.Drawing.Color.Blue;
+            this.lblHieght.Location = new System.Drawing.Point(262, 48);
+            this.lblHieght.Name = "lblHieght";
+            this.lblHieght.Size = new System.Drawing.Size(80, 25);
+            this.lblHieght.TabIndex = 5;
+            this.lblHieght.Text = "Hieght";
+            // 
+            // lblBattery
+            // 
+            this.lblBattery.AutoSize = true;
+            this.lblBattery.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBattery.ForeColor = System.Drawing.Color.Blue;
+            this.lblBattery.Location = new System.Drawing.Point(17, 157);
+            this.lblBattery.Name = "lblBattery";
+            this.lblBattery.Size = new System.Drawing.Size(72, 25);
+            this.lblBattery.TabIndex = 8;
+            this.lblBattery.Text = "Width";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(18, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Battery Information";
+            // 
+            // Battery_Life_Remaining
+            // 
+            this.Battery_Life_Remaining.AutoSize = true;
+            this.Battery_Life_Remaining.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Battery_Life_Remaining.ForeColor = System.Drawing.Color.Blue;
+            this.Battery_Life_Remaining.Location = new System.Drawing.Point(17, 265);
+            this.Battery_Life_Remaining.Name = "Battery_Life_Remaining";
+            this.Battery_Life_Remaining.Size = new System.Drawing.Size(72, 25);
+            this.Battery_Life_Remaining.TabIndex = 9;
+            this.Battery_Life_Remaining.Text = "Width";
+            // 
+            // Battery_Charge_Status
+            // 
+            this.Battery_Charge_Status.AutoSize = true;
+            this.Battery_Charge_Status.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Battery_Charge_Status.ForeColor = System.Drawing.Color.Blue;
+            this.Battery_Charge_Status.Location = new System.Drawing.Point(17, 229);
+            this.Battery_Charge_Status.Name = "Battery_Charge_Status";
+            this.Battery_Charge_Status.Size = new System.Drawing.Size(72, 25);
+            this.Battery_Charge_Status.TabIndex = 10;
+            this.Battery_Charge_Status.Text = "Width";
+            // 
+            // lblAcLine
+            // 
+            this.lblAcLine.AutoSize = true;
+            this.lblAcLine.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAcLine.ForeColor = System.Drawing.Color.Blue;
+            this.lblAcLine.Location = new System.Drawing.Point(17, 193);
+            this.lblAcLine.Name = "lblAcLine";
+            this.lblAcLine.Size = new System.Drawing.Size(72, 25);
+            this.lblAcLine.TabIndex = 11;
+            this.lblAcLine.Text = "Width";
+            // 
+            // Full_Battery_Lifetime
+            // 
+            this.Full_Battery_Lifetime.AutoSize = true;
+            this.Full_Battery_Lifetime.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Full_Battery_Lifetime.ForeColor = System.Drawing.Color.Blue;
+            this.Full_Battery_Lifetime.Location = new System.Drawing.Point(18, 301);
+            this.Full_Battery_Lifetime.Name = "Full_Battery_Lifetime";
+            this.Full_Battery_Lifetime.Size = new System.Drawing.Size(76, 25);
+            this.Full_Battery_Lifetime.TabIndex = 12;
+            this.Full_Battery_Lifetime.Text = "label4";
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(204, 383);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 37);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Close Computer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lblCloseComputer
+            // 
+            this.lblCloseComputer.AutoSize = true;
+            this.lblCloseComputer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCloseComputer.ForeColor = System.Drawing.Color.Blue;
+            this.lblCloseComputer.Location = new System.Drawing.Point(18, 389);
+            this.lblCloseComputer.Name = "lblCloseComputer";
+            this.lblCloseComputer.Size = new System.Drawing.Size(180, 25);
+            this.lblCloseComputer.TabIndex = 14;
+            this.lblCloseComputer.Text = "Close Computer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(466, 432);
+            this.Controls.Add(this.lblCloseComputer);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Full_Battery_Lifetime);
+            this.Controls.Add(this.lblAcLine);
+            this.Controls.Add(this.Battery_Charge_Status);
+            this.Controls.Add(this.Battery_Life_Remaining);
+            this.Controls.Add(this.lblBattery);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.lblHieght);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.Label lblHieght;
+        private System.Windows.Forms.Label lblBattery;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Battery_Life_Remaining;
+        private System.Windows.Forms.Label Battery_Charge_Status;
+        private System.Windows.Forms.Label lblAcLine;
+        private System.Windows.Forms.Label Full_Battery_Lifetime;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lblCloseComputer;
+    }
+}
+
---
+++ Changing_Wall_Paper/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Changing_Wall_Paper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Import the SystemParametersInfo function from user32.dll
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SystemParametersInfo(uint action, uint uParam, string vParam, uint winIni);
+
+        // Constants for the function
+        public static readonly uint SPI_SETDESKWALLPAPER = 0x14;
+        public static readonly uint SPIF_UPDATEINIFILE = 0x01;
+        public static readonly uint SPIF_SENDCHANGE = 0x02;
+
+        public static void SetWallpaper(string path)
+        {
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string wallpaperPath = openFileDialog1.FileName;
+                if (File.Exists(wallpaperPath))
+                {
+                    SetWallpaper(wallpaperPath);
+                    MessageBox.Show("Your wallpaper has been changed successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("The file does not exist.");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Import GetSystemMetrics from user32.dll
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetSystemMetrics(int nIndex);
+
+        // Getting Height and Width of Screen
+        public static void GettingHeightWidthOfScreen(Label lblWidth, Label lblHeight)
+        {
+            // Get the height and width of the screen
+            int screenWidth = GetSystemMetrics(0);
+            int screenHeight = GetSystemMetrics(1);
+            lblHeight.Text = screenHeight + " px";
+            lblWidth.Text = screenWidth + " px";
+        }
+
+        // Define the SYSTEM_POWER_STATUS structure
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SYSTEM_POWER_STATUS
+        {
+            public byte ACLineStatus;
+            public byte BatteryFlag;
+            public byte BatteryLifePercent;
+            public byte Reserved1;
+            public int BatteryLifeTime;
+            public int BatteryFullLifeTime;
+        }
+
+        // Import GetSystemPowerStatus from kernel32.dll
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool GetSystemPowerStatus(out SYSTEM_POWER_STATUS sps);
+
+        static string GetBatteryStatus(byte flag)
+        {
+            switch (flag)
+            {
+                case 1:
+                    return "High, more than 66% charged";
+                case 2:
+                    return "Low, less than 33% charged";
+                case 4:
+                    return "Critical, less than 5% charged";
+                case 8:
+                    return "Charging";
+                case 128:
+                    return "No battery";
+                case 255:
+                    return "Unknown status";
+                default:
+                    return "Battery status not detected";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GettingHeightWidthOfScreen(lblWidth, lblHieght);
+
+            if (GetSystemPowerStatus(out SYSTEM_POWER_STATUS status))
+            {
+                lblBattery.Text = "AC Line Status: " + (status.ACLineStatus == 0 ? "Offline" : "Online");
+                lblAcLine.Text = "Battery Charge Status: " + GetBatteryStatus(status.BatteryFlag);
+                Battery_Charge_Status.Text = "Battery Life Percent: " + (status.BatteryLifePercent == 255 ? "Unknown" : status.BatteryLifePercent + "%");
+                Battery_Life_Remaining.Text = "Battery Life Remaining: " + (status.BatteryLifeTime == -1 ? "Unknown" : status.BatteryLifeTime + " seconds");
+                Full_Battery_Lifetime.Text = "Full Battery Lifetime: " + (status.BatteryFullLifeTime == -1 ? "Unknown" : status.BatteryFullLifeTime + " seconds");
+            }
+            else
+            {
+                lblBattery.Text = "Unable to get battery status.";
+                lblAcLine.Text = "";
+                Battery_Charge_Status.Text = "";
+                Battery_Life_Remaining.Text = "";
+                Full_Battery_Lifetime.Text = "";
+            }
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool ExitWindowsEx(uint uFlags, uint dwReason);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool OpenProcessToken(IntPtr ProcessHandle, uint DesiredAccess, out IntPtr TokenHandle);
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool LookupPrivilegeValue(string lpSystemName, string lpName, out LUID lpLuid);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool AdjustTokenPrivileges(IntPtr TokenHandle, bool DisableAllPrivileges, ref TOKEN_PRIVILEGES NewState, int BufferLength, IntPtr PreviousState, IntPtr ReturnLength);
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct LUID
+        {
+            public uint LowPart;
+            public int HighPart;
+        }
+
+        public struct TOKEN_PRIVILEGES
+        {
+            public int PrivilegeCount;
+            public LUID Luid;
+            public int Attributes;
+        }
+
+        const int SE_PRIVILEGE_ENABLED = 0x00000002;
+        const int TOKEN_QUERY = 0x00000008;
+        const int TOKEN_ADJUST_PRIVILEGES = 0x00000020;
+        const uint EWX_LOGOFF = 0x00000000;
+        const uint EWX_SHUTDOWN = 0x00000001;
+        const uint EWX_REBOOT = 0x00000002;
+        const uint EWX_FORCE = 0x00000004;
+
+        static void EnableShutdownPrivilege()
+        {
+            if (!OpenProcessToken(System.Diagnostics.Process.GetCurrentProcess().Handle, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, out IntPtr tokenHandle))
+            {
+                MessageBox.Show("Failed to open process token.");
+                return;
+            }
+
+            if (!LookupPrivilegeValue(null, "SeShutdownPrivilege", out LUID luid))
+            {
+                MessageBox.Show("Failed to look up privilege value.");
+                return;
+            }
+
+            TOKEN_PRIVILEGES tp = new TOKEN_PRIVILEGES
+            {
+                PrivilegeCount = 1,
+                Luid = luid,
+                Attributes = SE_PRIVILEGE_ENABLED
+            };
+
+            if (!AdjustTokenPrivileges(tokenHandle, false, ref tp, 0, IntPtr.Zero, IntPtr.Zero))
+            {
+                MessageBox.Show("Failed to adjust token privileges.");
+                return;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            EnableShutdownPrivilege();
+
+            // Shutdown the system
+            if (!ExitWindowsEx(EWX_SHUTDOWN | EWX_FORCE, 0))
+            {
+                MessageBox.Show("Sorry, can't close it while doing other things. Try again after closing all apps.");
+            }
+        }
+    }
+}
